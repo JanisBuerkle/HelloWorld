3fc36b826a847a7ccc66a4e023220ee1615af96e
bks <bks@MAFBNB0502>
2023-09-11 14:03:31 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebe die 1. Zahl ein");
+            var ersteEingabe = Console.ReadLine();
+            Console.WriteLine("Gebe die 2. Zahl ein");
+            var zweiteEingabe = Console.ReadLine();
+            
+            int ersteZahl = Convert.ToInt32(ersteEingabe);
+            int zweiteZahl = Convert.ToInt32(zweiteEingabe);
+
+            int summe = ersteZahl + zweiteZahl;
+            Console.WriteLine("Dein Ergebnis ist: " + summe);
+            
+            Console.ReadKey();
+        }
+    }
+}
